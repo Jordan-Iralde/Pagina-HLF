328e8ca3bd1012339b992a4a46036dff6aa667fd
Jordan Iralde <iraldejordan10@gmail.com>
2025-02-26 21:04:24 -0300

    Angular

---
+++ PaginaHLF/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    RouterOutlet
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'PaginaHLF';
+}
---
+++ PaginaHLF/src/app/pages/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { LoginComponent } from '../login/login.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [
+    LoginComponent
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
